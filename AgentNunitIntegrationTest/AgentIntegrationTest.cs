fb7c69611f010dd3c859cb1d3e5726e4b3c05c45
Carlos Arreola <carlos.arreola@relativity.com>
2018-07-31 17:03:07 -0500

    fix integration test

--- AgentNunitIntegrationTest/AgentIntegrationTest.cs
+++ AgentNunitIntegrationTest/AgentIntegrationTest.cs
@@ -1,4 +1,5 @@
 ﻿using kCura.Relativity.Client;
+using kCura.Relativity.Client.DTOs;
 using NUnit.Framework;
 using Relativity.API;
 using Relativity.Test.Helpers;
@@ -6,6 +7,7 @@ using Relativity.Test.Helpers.ServiceFactory.Extentions;
 using Relativity.Test.Helpers.WorkspaceHelpers;
 using System;
 using System.Configuration;
+using System.Linq;
 
 namespace AgentNunitIntegrationTest
 {
@@ -16,9 +18,7 @@ namespace AgentNunitIntegrationTest
 
 		public IRSAPIClient _client;
 		public int _workspaceId;
-		public IDBContext WorkspaceDbConext;
 		public IServicesMgr ServicesManager;
-		public IDBContext EddsDbContext;
 		public const string NewFieldName = "Demo Document Field";
 		public string _workspaceName = string.Concat(ConfigurationManager.AppSettings["TestWorkspaceName"], Guid.NewGuid()).Substring(0,32);
 		public string _adminUsername = ConfigurationManager.AppSettings["AdminUsername"];
@@ -37,7 +37,6 @@ namespace AgentNunitIntegrationTest
 			//Create instance of Test Helper & set up services manager and db context
 			TestHelper helper = new TestHelper();
 			ServicesManager = helper.GetServicesManager();
-			WorkspaceDbConext = helper.GetDBContext(_workspaceId);
 
 			//Create client
 			_client = helper.GetServicesManager().GetProxy<IRSAPIClient>(_adminUsername, _adminPassword);
@@ -70,44 +69,41 @@ namespace AgentNunitIntegrationTest
 		public void Integration_Test_Golden_Flow_Valid()
 		{
 			//Arrange and Act
-			QueryResult fieldQueryResult = GetField(NewFieldName, _workspaceId, _client);
+			ResultSet<kCura.Relativity.Client.DTOs.Field> fieldQueryResult = GetField(NewFieldName, _workspaceId, _client);
 
 			//Assert
-			Assert.AreEqual(1, fieldQueryResult.TotalCount, "Count is not 1");
-			Assert.IsNotNull(fieldQueryResult.QueryArtifacts[0].ArtifactID, "ArtifactID should not be null");
+			Assert.That(fieldQueryResult.Results.Count, Is.EqualTo(1));
+			Assert.That(fieldQueryResult.Results.FirstOrDefault().Artifact.ArtifactID, Is.Not.Null);
+
+			Console.WriteLine($"Found {fieldQueryResult.Results.Count} field");
+			Console.WriteLine($"Name: {fieldQueryResult.Results.FirstOrDefault().Artifact.Name}");
+			Console.WriteLine($"Artifact ID: {fieldQueryResult.Results.FirstOrDefault().Artifact.ArtifactID}");
 		}
 
 		#endregion
 
 		#region privte helpers
 
-		public static QueryResult GetField(string fieldname, int workspaceID, IRSAPIClient _client)
+		private ResultSet<kCura.Relativity.Client.DTOs.Field> GetField(string fieldname, int workspaceID, IRSAPIClient _client)
 		{
 			_client.APIOptions.WorkspaceID = workspaceID;
-			
-			Query query = new Query();
+
+			Query<kCura.Relativity.Client.DTOs.Field> query = new Query<kCura.Relativity.Client.DTOs.Field>();
+			query.Fields = FieldValue.AllFields;
 			query.Condition = new TextCondition("Name", TextConditionEnum.Like, fieldname);
-			QueryResult result = null;
 
-			try
-			{
-				result = _client.Query(_client.APIOptions, query);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"An error occurred getting field {fieldname}: {ex.Message}");
-			}
+			ResultSet<kCura.Relativity.Client.DTOs.Field> results = _client.Repositories.Field.Query(query);
 
-			return result;
+			return results;
 		}
 
-		public static void DeleteField(string fieldname, int workspaceID, IRSAPIClient _client)
+		private void DeleteField(string fieldname, int workspaceID, IRSAPIClient _client)
 		{
 			_client.APIOptions.WorkspaceID = workspaceID;
-			QueryResult result = GetField(fieldname, workspaceID, _client);
+
 			try
 			{
-				_client.Repositories.Field.DeleteSingle(result.QueryArtifacts[0].ArtifactID);
+				_client.Repositories.Field.DeleteSingle(GetField(fieldname, workspaceID, _client).Results.FirstOrDefault().Artifact.ArtifactID);
 			}
 			catch (Exception ex)
 			{
